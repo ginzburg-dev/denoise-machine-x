4476595b1413a6013e7cac1c922a89cc19299970
ginzburg-dev <ginzburgdd@gmail.com>
2025-08-23 03:11:41 +0100

    refactor(buildinfo): remove compiler/system pragma messages

--- src/BuildInfo.cpp
+++ src/BuildInfo.cpp
@@ -8,22 +8,12 @@ namespace dmxdenoiser
     void logBuildBanner() {
 
         #if DMX_DEBUG_BUILD
-            #if defined(_MSC_VER)
-                #pragma message("Build mode: DEBUG (NDEBUG not defined)")
-            #else
-                #warning "Build mode: DEBUG (NDEBUG not defined)"
-            #endif
             constexpr const char* buildMode = "Debug";
         #else
-            #if defined(_MSC_VER)
-                #pragma message("Build mode: RELEASE (NDEBUG defined)")
-            #else
-                #warning "Build mode: RELEASE (NDEBUG defined)"
-            #endif
             constexpr const char* buildMode = "Release";
         #endif
 
-        DMX_LOG_INFO("BuildInfo", "\"", DMX_NAME, "\"(", DMX_SHORT_NAME, "), version: ",  DMX_VERSION, ", (", DMX_BUILD_DATE,"), ", buildMode );
+        DMX_LOG_INFO("BuildInfo", "\"", DMX_NAME, "\" (", DMX_SHORT_NAME, "), version: ",  DMX_VERSION, ", (", DMX_BUILD_DATE,"), build mode: ", buildMode );
     }
 
 } // namespace dmxdenoiser 
