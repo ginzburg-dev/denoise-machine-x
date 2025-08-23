8975af9155eed699907dd11940ffb32c23192ab5
ginzburg-dev <ginzburgdd@gmail.com>
2025-08-23 02:18:18 +0100

    refactor(logger): make init more robust handling log files; add localTime and shutdown methods

--- include/dmxdenoiser/Logger.hpp
+++ include/dmxdenoiser/Logger.hpp
@@ -40,24 +40,8 @@ namespace dmxdenoiser
             static Logger inst;
             return inst;
         }
-        
-        void init(LogLevel minLevel, std::ostream* os = nullptr, const std::string& filePath = "") {
-            std::lock_guard<std::mutex> lock{m_mutex};
-            m_minLevel = static_cast<int>(minLevel);
-            m_filePath = filePath;
-            m_out = os;
-            if (m_fileStream.is_open())       // close old file if any
-                m_fileStream.close();
 
-            if (!filePath.empty()) {
-                m_fileStream.open(filePath, std::ios::out | std::ios::app);
-            }
-        }
-
-        template<typename... Args>
-        void Log(LogLevel level, std::string_view tag, Args&&... args) {
-            if (static_cast<int>(level) < m_minLevel)
-                return;
+        static std::string localTime() {
             auto now = std::chrono::system_clock::now();
             auto t = std::chrono::system_clock::to_time_t(now);
             std::tm tm{};
@@ -68,6 +52,76 @@ namespace dmxdenoiser
             #endif
             char buf[24];
             std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &tm);
+            return std::string(buf);
+        }
+        
+        void init(LogLevel minLevel, std::ostream* os = nullptr, const std::string& filePath = "") {
+            std::lock_guard<std::mutex> lock{m_mutex};
+            m_initialized = true;
+            m_minLevel = static_cast<int>(minLevel);
+            m_filePath = filePath;
+            m_out = os;
+            if (m_fileStream.is_open())       // close old file if any
+            {
+                m_fileStream.flush();
+                m_fileStream.close();
+            }
+
+            if (!filePath.empty()) {
+                std::error_code ec;
+                std::filesystem::path path{filePath};
+                if (path.has_parent_path()) {
+                    // Create all missing parent directories (noop if they exist)
+                    std::filesystem::create_directories(path.parent_path(), ec);
+                    if (ec) {
+                        #if defined(DMX_DEBUG_BUILD)
+                            throw std::runtime_error("Cannot create log directory: " + path.parent_path().string());
+                        #else
+                            std::cerr << "["<< this->localTime() << "]: [ERROR] [Logger]: Cannot create log directory: " << path.parent_path().string();
+                        #endif
+                    }
+                }
+
+                m_fileStream.open(filePath, std::ios::out | std::ios::app);
+                if (!m_fileStream) {
+                    #if defined(DMX_DEBUG_BUILD)
+                        throw std::runtime_error("Failed to open log file: " + path.string());
+                    #else
+                        std::cerr << "["<< this->localTime() << "]: [ERROR] [Logger]: Cannot create log directory: " << path.parent_path().string();
+                    #endif
+                }
+            }
+        }
+
+        void shutdown() {
+            std::lock_guard<std::mutex> lock{m_mutex};
+            if (m_fileStream.is_open())
+            {
+                m_fileStream.flush();
+                m_fileStream.close();
+            }
+            m_initialized = false;
+            m_filePath = "";
+            m_out =nullptr;
+        }
+
+        template<typename... Args>
+        void Log(LogLevel level, std::string_view tag, Args&&... args) {
+            if (static_cast<int>(level) < m_minLevel)
+                return;
+            /*
+            auto now = std::chrono::system_clock::now();
+            auto t = std::chrono::system_clock::to_time_t(now);
+            std::tm tm{};
+            #if defined(_WIN32)
+                localtime_s(&tm, &t);
+            #else
+                localtime_r(&t, &tm);
+            #endif
+            char buf[24];
+            std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &tm);
+            */
+            std::string localTime_{this->localTime()};
 
             // Build message text once (handles empty varargs too), with neat spacing
             std::ostringstream msg;
@@ -78,7 +132,7 @@ namespace dmxdenoiser
             std::lock_guard<std::mutex> lock(m_mutex);
             
             auto out_one = [&](std::ostream& os) {
-                os  << "[" << buf << "]: "
+                os  << "[" << localTime_ << "]: "
                     << "[" << ToString(level) << "] "
                     << "[" << tag << "] "
                     << line << '\n';
@@ -88,6 +142,8 @@ namespace dmxdenoiser
             if (m_fileStream.is_open()) { out_one(m_fileStream); m_fileStream.flush(); }
         }
 
+        bool isInitialized() const { return m_initialized; }
+
         ~Logger() noexcept { m_fileStream.close(); }
 
     private:
@@ -96,6 +152,7 @@ namespace dmxdenoiser
         std::ostream* m_out = nullptr;
         std::ofstream m_fileStream;
         int m_minLevel{ static_cast<int>(LogLevel::Info) };
+        bool m_initialized{false};
 
         Logger() = default;
         Logger(const Logger&) = delete;
@@ -110,7 +167,7 @@ namespace dmxdenoiser
 // Initialize logger
 #define DMX_LOG_INIT(LOGLEVEL, OSTREAMPTR, FILENAME) \
     dmxdenoiser::Logger::instance().init((LOGLEVEL), (OSTREAMPTR), (FILENAME)); \
-    dmxdenoiser::Logger::instance().Log(dmxdenoiser::LogLevel::Info, "Logger", "Logging system initialized")
+    dmxdenoiser::Logger::instance().Log(dmxdenoiser::LogLevel::Debug, "Logger", "Logging system initialized")
 
 #if DMX_DEBUG_BUILD
     #define DMX_LOG_TRACE(tag, ...) dmxdenoiser::Logger::instance().Log(dmxdenoiser::LogLevel::Trace, (tag), ##__VA_ARGS__)
@@ -128,3 +185,5 @@ namespace dmxdenoiser
 #define DMX_LOG_WARNING(tag, ...) dmxdenoiser::Logger::instance().Log(dmxdenoiser::LogLevel::Warning, (tag), ##__VA_ARGS__)
 #define DMX_LOG_ERROR(tag, ...) dmxdenoiser::Logger::instance().Log(dmxdenoiser::LogLevel::Error, (tag), ##__VA_ARGS__)
 #define DMX_LOG_CRITICAL(tag, ...) dmxdenoiser::Logger::instance().Log(dmxdenoiser::LogLevel::Critical, (tag), ##__VA_ARGS__)
+
+#define DMX_LOG_SHUTDOWN dmxdenoiser::Logger::instance().shutdown()
