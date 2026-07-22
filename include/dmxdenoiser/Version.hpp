66ec8ef0fd4ad40e7f51df4addffdf5b74af3b4f
ginzburg-dev <ginzburgdd@gmail.com>
2026-07-22 16:12:10 +0100

    update version

--- include/dmxdenoiser/Version.hpp
+++ include/dmxdenoiser/Version.hpp
@@ -6,10 +6,10 @@
 
 namespace dmxdenoiser {
 
-    inline constexpr const char* kProductName = "denoise-machine-x";
-    inline constexpr const char* kProductShortName = "dmxdenoiser";
-    inline constexpr const char* kBuildVersion = "0.1.0";
-    inline constexpr const char* kBuildDate = __DATE__ " " __TIME__;
+inline constexpr const char* kProductName = "denoise-machine-x";
+inline constexpr const char* kProductShortName = "dmxdenoiser";
+inline constexpr const char* kBuildVersion = "0.1.0";
+inline constexpr const char* kBuildDate = __DATE__ " " __TIME__;
 
-}
+} // namespace dmxdenoiser
 
